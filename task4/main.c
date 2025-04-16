757beec4c363b273016c2401cbbf74ab9635cf6b
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-16 19:19:31 +0400

    fixed

--- task4/main.c
+++ task4/main.c
@@ -3,8 +3,8 @@
 char* copy(char*, char*);
 
 int main(){
-	char str1[4] = {};
-	char str2[40] = {};;
+	char str1[20] = {};
+	char str2[5] = {};;
 	fgets(str2, sizeof(str2), stdin);
 	copy(str1, str2);
 }
