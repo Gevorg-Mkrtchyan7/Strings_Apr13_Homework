ec7bd855d021c6d9225e960aac8248c70b78b4e7
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-16 15:53:11 +0400

    tnayin

--- task4/main.c
+++ task4/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void copy(char*, char*);
+char* copy(char*, char*);
 
 int main(){
 	char str1[4] = {};
-	char str2[20] = {};
-	fgets(str1, sizeof(str1), stdin);
+	char str2[40] = {};;
+	fgets(str2, sizeof(str2), stdin);
 	copy(str1, str2);
 }
--- task4/task4.c
+++ task4/task4.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
-void copy(const char* str1, char* str2){
-	int a = strlen(str2);
-	strncpy(str2, str1, a);
-	return str1;
+void copy(char* str1, const char* str2){
+	int i = 0;
+	while(i < sizeof(str2) && str2[i] != '\0'){
+		str1[i] = str2[i];
+		i++;
+	}
+	str1[i] = '\0';
+	printf("%s \n", str1);
 }
